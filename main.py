d658daf9aa1eddeaf1a4c1d01e5e1580a09c624b
Martin Chaperot-Merino <35586618+tinmarr@users.noreply.github.com>
2019-01-23 10:39:33 -0800

    Fixed Issue#1

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import numpy as np
+from sys import exit
 
+vowels = np.array(["a","e","i","o","u"])
 
-print("Welcome to the Great Translator! \nType exit at any time to leave.")
+print("Welcome to the Great Translator!\nType exit at any time to leave.")
 while True:
     start = input("Enter a phrase:")
     if start == "exit":
-        break
+        exit()
     n = start.split()
     n = np.array(n)
     p = []
+    v = []
     for i in range(len(n)):
         k = n[i]
-        k = k[0]
+        for j in range(len(k)):
+          if k[j] in vowels:
+            v.append(j)
+        k = k[:v[i]]
         p.append(k)
     l = np.array(p)
-    j = l[len(l)-1]
+    m = l[len(l)-1]
     l = np.delete(l,(len(l)-1))
-    l = np.insert(l,0,j)
+    l = np.insert(l,0,m)
     final = []
     for i in range(len(n)):
         k = n[i]
-        k = k[1:]
+        k = k[v[i]:]
         k = list(k)
         k.insert(0,l[i])
         k = "".join(k)
